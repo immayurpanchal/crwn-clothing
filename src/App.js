940b9ad1de40c70129b6ddb73faed92afc86ac84
Mayur Panchal <mayurpanchal002@gmail.com>
2020-07-05 22:32:28 +0530

    Created homepage and added required styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import Homepage from './pages/Homepage';
 
 function App() {
-	return <div className='App'></div>;
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
