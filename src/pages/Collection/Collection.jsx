0a585663be977c567f9fbbac938c701fc6876222
Mayur Panchal <mayurpanchal002@gmail.com>
2020-08-28 22:38:58 +0530

    converted CollectionItem, Collection into styled component

--- src/pages/Collection/Collection.jsx
+++ src/pages/Collection/Collection.jsx
@@ -2,20 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { selectCollection } from '../../redux/shop/shop.selector';
 import CollectionItem from '../../components/CollectionItem';
-import './Collection.scss';
+import {
+  CollectionPageContainer,
+  CollectionTitle,
+  CollectionItemsContainer
+} from './Collection.styles';
 
 const Collection = (props) => {
   const { collection } = props;
   const { title, items } = collection;
   return (
-    <div className="collection-page">
-      <h2 className="title">{title}</h2>
-      <div className="items">
+    <CollectionPageContainer>
+      <CollectionTitle>{title}</CollectionTitle>
+      <CollectionItemsContainer>
         {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
-    </div>
+      </CollectionItemsContainer>
+    </CollectionPageContainer>
   );
 };
 
