1db8a31719d77e2b4576e94c9cad4f683fedcf4a
Mayur Panchal <mayurpanchal002@gmail.com>
2020-08-30 18:46:46 +0530

    misc updates

--- src/components/StripeButton/StripeButton.jsx
+++ src/components/StripeButton/StripeButton.jsx
@@ -3,45 +3,45 @@ import StripeCheckout from 'react-stripe-checkout';
 import axios from 'axios';
 
 const StripeButton = (props) => {
-  const { price } = props;
-  const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_wKdTXJVjDKfLeNCkPK0pTFJk00RwisgQeD';
+	const { price } = props;
+	const priceForStripe = price * 100;
+	const publishableKey = 'pk_test_wKdTXJVjDKfLeNCkPK0pTFJk00RwisgQeD';
 
-  // Pass this token to backend for successful payment
-  const onToken = (token) => {
-    axios({
-      url: 'payment',
-      method: 'POST',
-      data: {
-        amount: priceForStripe,
-        token
-      }
-    })
-      .then((res) => {
-        alert('Payment Successful!');
-      })
-      .catch((err) => {
-        console.log('Payment Error: ');
-        console.log(err);
-        alert('There was an issue for the payment. Please sure you use the provided credit card');
-      });
-  };
-  return (
-    <>
-      <StripeCheckout
-        label="Pay Now"
-        name="CRWN Clothing Ltd."
-        billingAddress
-        shippingAddress
-        image="https://svgshare.com/i/CUz.svg"
-        description={`Your Total is $${price}`}
-        amount={priceForStripe}
-        panelLabel="Pay Now"
-        token={onToken}
-        stripeKey={publishableKey}
-      />
-    </>
-  );
+	// Pass this token to backend for successful payment
+	const onToken = (token) => {
+		axios({
+			url: 'payment',
+			method: 'POST',
+			data: {
+				amount: priceForStripe,
+				token,
+			},
+		})
+			.then((res) => {
+				alert('Payment Successful!');
+			})
+			.catch((err) => {
+				console.log('Payment Error: ');
+				console.log(err);
+				alert('There was an issue for the payment. Please sure you use the provided credit card');
+			});
+	};
+	return (
+		<>
+			<StripeCheckout
+				label='Pay Now'
+				name='CRWN Clothing Ltd.'
+				billingAddress
+				shippingAddress
+				image='https://svgshare.com/i/CUz.svg'
+				description={`Your Total is $${price}`}
+				amount={priceForStripe}
+				panelLabel='Pay Now'
+				token={onToken}
+				stripeKey={publishableKey}
+			/>
+		</>
+	);
 };
 
 export default StripeButton;
