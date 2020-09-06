cd992e9168f6d83303bee946ae5437b74a4feaad
Mayur Panchal <mayurpanchal002@gmail.com>
2020-09-06 11:36:04 +0530

    improved performance by adding react lazy, memo, error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,72 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.scss';
 import Header from './components/Header/Header';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-import Homepage from './pages/Homepage';
-import Shop from './pages/Shop/Shop';
-import SignInAndSignUpPage from './pages/SignInAndSignUpPage';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selector';
 import { createStructuredSelector } from 'reselect';
-import Checkout from './pages/Checkout';
+import Spinner from './components/Spinner';
+import ErrorBoundary from './components/ErrorBoundary';
+
+const Homepage = lazy(() => import('./pages/Homepage'));
+const Shop = lazy(() => import('./pages/Shop'));
+const Checkout = lazy(() => import('./pages/Checkout'));
+const SignInAndSignUpPage = lazy(() => import('./pages/SignInAndSignUpPage'));
 
 const App = (props) => {
-  const { currentUser, setCurrentUser } = props;
-  useEffect(() => {
-    const unscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-      // if user is logged in via firebase, userAuth contains details of the user
-      if (userAuth) {
-        // create user profile in firebase DB and get the userRef
-        const userRef = await createUserProfileDocument(userAuth);
+	const { currentUser, setCurrentUser } = props;
+	useEffect(() => {
+		const unscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+			// if user is logged in via firebase, userAuth contains details of the user
+			if (userAuth) {
+				// create user profile in firebase DB and get the userRef
+				const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot((snapShot) => {
-          // snapshot.data() contains the user data stored in the firestore DB
-          // snapshot.id contains the unique id
-          setCurrentUser({ id: snapShot.id, ...snapShot.data() });
-        });
-      } else {
-        // useAuth=null in case user is not logged in
-        setCurrentUser(userAuth);
-      }
-    });
+				userRef.onSnapshot((snapShot) => {
+					// snapshot.data() contains the user data stored in the firestore DB
+					// snapshot.id contains the unique id
+					setCurrentUser({ id: snapShot.id, ...snapShot.data() });
+				});
+			} else {
+				// useAuth=null in case user is not logged in
+				setCurrentUser(userAuth);
+			}
+		});
 
-    return () => unscribeFromAuth();
-  }, []);
+		return () => unscribeFromAuth();
+	}, []);
 
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route path="/shop" component={Shop} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route
-          path="/signin"
-          render={() => (currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />)}
-        />
-      </Switch>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<Switch>
+				<ErrorBoundary>
+					<Suspense fallback={<Spinner />}>
+						<Route exact path='/' component={Homepage} />
+						<Route path='/shop' component={Shop} />
+						<Route exact path='/checkout' component={Checkout} />
+						<Route
+							path='/signin'
+							render={() =>
+								currentUser ? <Redirect to='/' /> : <SignInAndSignUpPage />
+							}
+						/>
+					</Suspense>
+				</ErrorBoundary>
+			</Switch>
+		</div>
+	);
 };
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser
+	currentUser: selectCurrentUser,
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user))
+	setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -2,51 +2,54 @@ import React from 'react';
 
 import './Checkout.scss';
 import { createStructuredSelector } from 'reselect';
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selector';
+import {
+	selectCartItems,
+	selectCartTotal,
+} from '../../redux/cart/cart.selector';
 import { connect } from 'react-redux';
 import CheckoutItem from '../../components/CheckoutItem/CheckoutItem';
 import StripeButton from '../../components/StripeButton/StripeButton';
 
 const Checkout = (props) => {
-  const { cartItems, total } = props;
-  return (
-    <div className="checkout-page">
-      <div className="checkout-header">
-        <div className="header-block">
-          <span>Product</span>
-        </div>
-        <div className="header-block">
-          <span>Description</span>
-        </div>
-        <div className="header-block">
-          <span>Quantity</span>
-        </div>
-        <div className="header-block">
-          <span>Price</span>
-        </div>
-        <div className="header-block">
-          <span>Remove</span>
-        </div>
-      </div>
-      {cartItems.map((cartItem) => (
-        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-      ))}
-      <div className="total">
-        <span>TOTAL: ${total}</span>
-      </div>
-      <div className="test-warning">
-        **Please use the test credit card for Payment
-        <br />
-        4242 4242 4242 4242 - Exp: 01/26 - CVV : 123
-      </div>
-      <StripeButton price={total} />
-    </div>
-  );
+	const { cartItems, total } = props;
+	return (
+		<div className='checkout-page'>
+			<div className='checkout-header'>
+				<div className='header-block'>
+					<span>Product</span>
+				</div>
+				<div className='header-block'>
+					<span>Description</span>
+				</div>
+				<div className='header-block'>
+					<span>Quantity</span>
+				</div>
+				<div className='header-block'>
+					<span>Price</span>
+				</div>
+				<div className='header-block'>
+					<span>Remove</span>
+				</div>
+			</div>
+			{cartItems.map((cartItem) => (
+				<CheckoutItem key={cartItem.id} cartItem={cartItem} />
+			))}
+			<div className='total'>
+				<span>TOTAL: ${total}</span>
+			</div>
+			<div className='test-warning'>
+				**Please use the test credit card for Payment
+				<br />
+				4242 4242 4242 4242 - Exp: 01/26 - CVV : 123
+			</div>
+			<StripeButton price={total} />
+		</div>
+	);
 };
 
 const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems,
-  total: selectCartTotal
+	cartItems: selectCartItems,
+	total: selectCartTotal,
 });
 
 export default connect(mapStateToProps)(Checkout);
